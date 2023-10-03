bb1bf437b95f070424f57b53ff727f2676ac5adc
nagasawa <c0a2210739@edu.teu.ac.jp>
2023-10-03 14:48:40 +0900

    3回目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400)  #こうかとん初期座標
     enn = pg.Surface((20,20))
     pg.draw.circle(enn,(255,0,0),(10,10),10)
     enn.set_colorkey((0,0,0))
@@ -23,20 +25,28 @@ def main():
     vy = 5
     clock = pg.time.Clock()
     tmr = 0
+    kk_dic = {pg.K_UP:(0,-5),pg.K_DOWN:(0,5),pg.K_LEFT:(-5,0),pg.K_RIGHT:(5,0)}
+    key_lst = pg.key.get_pressed()
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        screen.blit(enn,bc_rct)
-        bc_rct.move_ip(vx,vy)
 
-        
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for key,mv in kk_dic.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]   #横合計移動量
+                sum_mv[1] += mv[1]   #縦
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+        screen.blit(enn,bc_rct)
+        bc_rct.move_ip(vx,vy)        
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
